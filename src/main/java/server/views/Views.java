782e0f20a8e614f2ee2f55c8ca15d28b0e574db7
funa-dev <funahashi.ryouma@gmail.com>
2026-09-12 22:26:24 +0900

    feat: implement document editing and workspace views with update and delete functionality

--- src/main/java/server/views/Views.java
+++ src/main/java/server/views/Views.java
@@ -3,6 +3,8 @@ package server.views;
 public final class Views {
     public static final String INDEX = "index";
     public static final String DETAIL = "detail";
+    public static final String EDIT = "edit";
+    public static final String WORKSPACE = "workspace";
     public static final String REDIRECT_ROOT = "redirect:/";
 
     private Views() {}
